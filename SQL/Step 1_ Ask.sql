e8ca84a36ed82ff6b4e8f33e80801e4618805ebb
ssagastume11 <sagassergio123@gmail.com>
2026-03-04 11:26:58 -0500

    Rename Step 1_Ask.sql to Step 1_ Ask.sql

--- SQL/Step 1_ Ask.sql
+++ SQL/Step 1_ Ask.sql
@@ -1,1 +1,10 @@
+-- Business Task:
+-- Analyze demographic data of Oscar winners to uncover trends in representation across key categories (Best Actor, Best Actress, Best Supporting Actor, Best Supporting Actress, and Best Director) and evaluate how diversity has changed throughout the history of the Academy Awards.
 
+-- Key Questions:
+-- 1. How has racial and ethic representation among winners changed since 1928?
+-- 2. What are the most common demographics characteristics of Oscar winners?
+-- 3. Has the average age winners shifted over time
+-- 4. Are certain demographic groups more represented in specific award categories?
+-- 5. Are there noticeable changes in diversity trends across decades?
+-- 6. How does birthplace or nationality distribution vary across awards types?
